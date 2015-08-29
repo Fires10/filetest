764feb4e5954a741967b34ff538397893c6c0ca0
Fires10 <Fires10@gmail.com>
2015-08-29 06:33:00 -0400

    Update test2.c
    more stuff added

--- test2.c
+++ test2.c
@@ -1,3 +1,22 @@
 /* trying to read an entire file into memory */
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+FILE *tempsensor;
+FILE *output;
+
+
+
+int main(int args, char *argv[])
+{
+    tempsensor = fopen( argv[1], "r" );
+
+    /* fopen returns 0, the NULL pointer, on failure */
+    if ( tempsensor == 0 )
+    {
+        printf( "Could not open Temperature Sensor\n" );
+    }
+
+}
