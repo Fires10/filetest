9332a30cb2434ad329d51ab21c45df5eee97e58f
Fires10 <Fires10@gmail.com>
2015-08-29 09:49:41 -0400

    Update test2.c
    added more

--- test2.c
+++ test2.c
@@ -6,17 +6,32 @@
 
 FILE *tempsensor;
 FILE *output;
+char * mydata;
 
 
 
 int main(int args, char *argv[])
 {
-    tempsensor = fopen( argv[1], "r" );
+    tempsensor = fopen( argv[1], "rb" );
 
     /* fopen returns 0, the NULL pointer, on failure */
     if ( tempsensor == 0 )
     {
         printf( "Could not open Temperature Sensor\n" );
     }
+  
+
+
+
+
+fseek(tempsensor, 0, SEEK_END);
+long fsize = ftell(tempsensor);
+rewind(tempsensor);
+
+mydata = malloc(fsize + 1);
+fread(mydata, fsize, 1, tempsensor);
+fclose(tempsensor);
+
+mydata[fsize] = 0;
 
 }
